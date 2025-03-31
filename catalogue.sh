14f44aa5d7ddcb6f626b136bc6e232dfcf5426c8
vinay <vinaybkrishna10@gmail.com>
2025-03-31 20:32:14 +0530

    Commmmiting mmy first commit

--- catalogue.sh
+++ catalogue.sh
@@ -1,7 +1,39 @@
 source common.sh
 component="catalogue"
 
-nodejs_setup
+cp catalogue.service /etc/systemd/system/catalogue.service
+
+print_head node disable
+dnf module disable nodejs -y
+echo $?
+
+print_head node enable
+dnf module enable nodejs:20 -y
+echo $?
+
+print_head install nodejs
+dnf install nodejs -y
+echo $?
+
+print_head user added
+useradd roboshop
+
+print_head /APP DIREC created
+mkdir /app
+
+print_head file modified
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+cd /app
+unzip /tmp/catalogue.zip
+
+cd /app
+npm install
+
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
+
+
 
 print_head install mongodb-mongosh
 dnf install mongodb-mongosh -y
