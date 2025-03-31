32cde2393f0ceaa211d582d3a643eed09b21313e
vinay <vinaybkrishna10@gmail.com>
2025-03-31 18:13:39 +0530

    Commmmiting mmy first commit

--- frontend.sh
+++ frontend.sh
@@ -4,17 +4,24 @@ component="nginx"
 print_head Disable Default Nginx
 dnf module disable nginx -y >> log_file
 
-
+print_head enable Nginx
 dnf module enable nginx:1.24 -y >> log_file
 
 
+print_head install Nginx
 dnf install nginx -y >> log_file
 
+print_head copy files
 cp nginx.conf /etc/nginx/nginx.conf >> log_file
 
+print_head remove old file
 rm -rf /usr/share/nginx/html/*
+
+print_head download main file
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip >> log_file
 cd /usr/share/nginx/html
+
+print_head unzip to folder
 unzip /tmp/frontend.zip >> log_file
 
 
