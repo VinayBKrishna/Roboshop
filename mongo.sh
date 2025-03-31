5c03c7574d18b68e9f608b7780bbcd67dfbf3dcd
vinay <vinaybkrishna10@gmail.com>
2025-03-31 20:11:48 +0530

    Commmmiting mmy first commit

--- mongo.sh
+++ mongo.sh
@@ -7,6 +7,9 @@ cp mongod.repo /etc/yum.repos.d/mongo.repo
 print_head Install mongodb
 dnf install mongodb-org -y
 
+systemctl enable mongod
+systemctl start mongod
+
 print_head ip address
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 
