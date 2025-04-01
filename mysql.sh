9e4ffe1717d0ada5c8b9a294d9ba989d3b4efa5b
vinay <vinaybkrishna10@gmail.com>
2025-04-01 15:44:31 +0530

    Commmmiting mmy first commit

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,11 @@
+print_head install mysql
 dnf install mysql-server -y
+echo$?
 
+print_head system enable
 systemctl enable mysqld
 systemctl start mysqld
 
+print_head setting password for the data base
 mysql_secure_installation --set-root-pass RoboShop@1
+echo $?
