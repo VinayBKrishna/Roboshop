92e70b21096281b8eae731cce37d866df6a447cb
vinay <vinaybkrishna10@gmail.com>
2025-04-01 15:21:30 +0530

    Commmmiting mmy first commit

--- user.sh
+++ user.sh
@@ -1,4 +1,35 @@
 source common.sh
 component="user"
 
-nodejs_setup
+cp user.service /etc/systemd/system/user.service
+
+print_head node disable
+dnf module disable nodejs -y
+
+print_head node enable
+dnf module enable nodejs:20 -y
+echo $?
+
+dnf install nodejs -y
+echo $?
+
+print_head user added
+useradd roboshop
+
+print_head /APP DIREC created
+mkdir /app
+
+print_head file modified
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
+cd /app
+unzip /tmp/user.zip
+echo $?
+
+print_head npmm install
+cd /app
+npm install
+echo $?
+
+systemmd_setup
+systemctl status user
+
