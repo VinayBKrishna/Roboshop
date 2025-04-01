058d484a1763879665095d1cd1aa60e0ec459f91
vinay <vinaybkrishna10@gmail.com>
2025-04-01 16:06:37 +0530

    Commmmiting mmy first commit

--- rabbitMq.sh
+++ rabbitMq.sh
@@ -1,9 +1,19 @@
 dnf install rabbitmq-server -y
 
+print_head copiny files
 cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+  echo $?
 
+print_head enabeling
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
+  echo $?
 
+print_head status
+systemctl status rabbitmq-server
+  echo $?
+
+print_head adding and setting permissions
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+  echo $?
