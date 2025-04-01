af1b72b0f91c51c03a15d0caa4a2e20b9ca35a5f
vinay <vinaybkrishna10@gmail.com>
2025-04-01 16:34:22 +0530

    Commmmiting mmy first commit

--- cart.sh
+++ cart.sh
@@ -2,22 +2,37 @@ source common.sh
 component="cart"
 
 cp cart.service /etc/systemd/system/cart.service
+  echo $?
 
 print_head node disabled
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
+  echo $?
 
 print_head nodejs installed
 dnf install nodejs -y
+  echo $?
 
-
-
+print_head user added
 useradd roboshop
+  echo $?
 
-artificat_download
+mkdir /app
 
+print_head files extration
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
+cd /app
+unzip /tmp/cart.zip
+  echo $?
+
+print_head npm install
 cd /app
 npm install
+  echo $?
+
 
 systemmd_setup
+
+print_head nodejs installed
 systemctl status cart
+  echo $?
