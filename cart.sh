a1b05a0c5d7454c6155f1a03d91e6b323358d794
vinay <vinaybkrishna10@gmail.com>
2025-04-01 15:29:25 +0530

    Commmmiting mmy first commit

--- cart.sh
+++ cart.sh
@@ -1,12 +1,16 @@
 source common.sh
 component="cart"
 
+cp cart.service /etc/systemd/system/cart.service
+
+print_head node disabled
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 
+print_head nodejs installed
 dnf install nodejs -y
 
-cp cart.service /etc/systemd/system/cart.service
+
 
 useradd roboshop
 
@@ -16,3 +20,4 @@ cd /app
 npm install
 
 systemmd_setup
+systemctl status cart
