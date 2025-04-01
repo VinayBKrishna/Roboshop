48438d4791b60cbd35369ebcf20d1885e844157a
vinay <vinaybkrishna10@gmail.com>
2025-04-01 15:15:42 +0530

    Commmmiting mmy first commit

--- Redis.sh
+++ Redis.sh
@@ -1,12 +1,17 @@
 source common.sh
+
+print_head Disable redis
 dnf module disable redis -y
 dnf module enable redis:7 -y
+echo $?
 
 print_head install redis
 dnf install redis -y
+echo $?
 
 print_head update redis config file
 sed -i -e 's|127.0.0.1|0.0.0.0|' -e '/protected-mode/ c protected-mode no' /etc/redis.conf
+echo $?
 
 systemctl enable redis
 systemctl start redis
