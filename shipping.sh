3bd92017b98d3c0c535ccfd4e9b9eb84c0b31938
vinay <vinaybkrishna10@gmail.com>
2025-04-03 16:10:36 +0530

    Commmmiting mmy first commit

--- shipping.sh
+++ shipping.sh
@@ -4,11 +4,15 @@ component="shipping"
 print_head maven setup
 maven_setup
 
-mysql -h mysql-dev.mikeydevops1.online -uroot -pRoboShop@1 < /app/db/schema.sql
+for file in schema app-user master-data;do
+  mysql -h mysql-dev.mikeydevops1.online -uroot -pRoboShop@1 < /app/db/${file}.sql
+done
 
-mysql -h mysql-dev.mikeydevops1.online -uroot -pRoboShop@1 < /app/db/app-user.sql
-
-mysql -h mysql-dev.mikeydevops1.online -uroot -pRoboShop@1 < /app/db/master-data.sql
+#mysql -h mysql-dev.mikeydevops1.online -uroot -pRoboShop@1 < /app/db/schema.sql
+#
+#mysql -h mysql-dev.mikeydevops1.online -uroot -pRoboShop@1 < /app/db/app-user.sql
+#
+#mysql -h mysql-dev.mikeydevops1.online -uroot -pRoboShop@1 < /app/db/master-data.sql
 
 
 
